b0281744315610847f619b1a779da265ad350f76
Paul Johnson <paul@johnson.kiwi.nz>
2015-10-18 20:09:33 +1300

    WIP on slides.

--- monads.js
+++ monads.js
@@ -1,3 +1,7 @@
+var chai = require('chai');
+
+chai.should();
+
 /*slide
 layout: true
 class: center, middle
@@ -26,7 +30,7 @@ Slides in markdown, code and tests in one file.
 
 *"In addition to it begin useful, it is also cursed and the curse of the monad
 is that once you get the epiphany, once you understand - "oh that's what it is"
-- you lose the ability to explain it to anybody."*
+\- you lose the ability to explain it to anybody."*
 
 **- Douglas Crockford**
 ???
@@ -67,43 +71,128 @@ Covered much more thorougly in
 */
 
 /*slide
-Example 1. Regular asynchronous/callback io.
+#Example 1. Promises.
+
+Asynchronous I/O.
 ???
 Should be pretty familiar to everyone.
 */
 
-function example_1_normal(cb) {
-    // TODO
+function async_io_normal(cb) {
+
 }
 
-describe("example 1 normal", function(done){
+describe("async io normal", function(done){
 
 });
 
 /*slide
-{example_1_normal}
+%(async_io_normal)s
 */
 
-function example_1_monad() {
-    // TODO
+function async_io_monad() {
+
 }
 
-describe("example 1 normal", function(done){
+describe("async io monad", function(done){
 
 });
 
 /*slide
-{example_1_monad}
+%(async_io_monad)s
 ???
-What pr
+Standard promises.
+
+Meets the definition we had earlier:
+Promise contains a value (in the future) or possibly a failure.
+'then' function takes the value and
 */
 
 /*slide
-Example 3. Arrays/lists.
+#Example 2. Option.
+
+???
+Present in many languages now, most people might be aware of it in Swift.
+*/
+
+
+function User(username, password, password_confirmation) {
+    this.username = username;
+    this.password = password;
+    this.password_confirmation = password_confirmation;
+}
+
+function has_params_normal(params) {
+    var username = params['username'];
+    if (username) {
+        var password = params['password'];
+        if (password) {
+            var password_confirmation = params['password_confirmation']
+            if (password_confirmation) {
+                return "has all required params";
+            }
+        }
+    }
+
+    return null;
+}
+
+describe("validate login normal", function(done){
+
+});
+
+/*slide
+%(check_registration_normal)s
+*/
+
+function Some(value) {
+    this.value = value;
+}
+
+Some.prototype.then = function(func) {
+    return func(this.value);
+}
+
+var None = {
+    then: function(func) {
+        return this;
+    }
+}
+
+// Probably a bad idea to extend Object, but makes things easier.
+Object.prototype.has_attr = function(attribute) {
+    if (this.hasOwnProperty(attribute)) {
+        return new Some(attribute);
+    } else {
+        return None;
+    }
+}
+
+function has_params_monad(params) {
+    return params.has_attr('username')
+        .then((username) => params.has_attr('password'))
+        .then((password) => params.has_attr('password_confirmation'))
+        .then((password_confirmation) => new Some("has all required params"));
+}
+
+describe("validate login monad", function(done){
+
+});
+
+/*slide
+%(has_params_monad)s
+???
+Definition:
+- Contains a value (Some) or nothing (None).
+- functions that take value and return a new container.
+- functions that take the container and then calls the value function if it's
+not none.
+*/
+
+/*slide
+#Example 3. Arrays/lists.
 
 Getting all the items a user has ever purchased.
-???
-Should be pretty familiar to everyone.
 */
 
 function get_users_items_normal(users) {
@@ -111,7 +200,7 @@ function get_users_items_normal(users) {
     for (user of users) {
         for (order of user.orders) {
             for (item of order.items) {
-
+                items.push(item);
             }
         }
     }
@@ -119,83 +208,54 @@ function get_users_items_normal(users) {
     return items;
 }
 
-var user_fixture = {
-    name: "Bob",
-    orders: [
-        {
-            id: 1,
-            items: [
-                {
-
-                },
-                {
-
-                }
-            ]
-        },
-        {
-            id: 1,
-            items: [
-                {
-
-                },
-                {
-
-                }
-            ]
-        },
-        {
-            id: 1,
-            items: [
-                {
-
-                },
-                {
-
-                }
-            ]
-        },
-    ]
-}
-
-describe("get user's items normal", function(done){
-
-});
-
 /*slide
-{get_users_items_normal}
+%(get_users_items_normal)s
 */
 
 Array.prototype.then = function(func) {
-    for (item of this) {
-
+    var x = [];
+    for (i of this) {
+        x.push(func(i));
     }
 
+    return x;
 }
 
-function get_users_items_monad() {
-    return user_fixture.users
+function get_users_items_monad(users) {
+    return users
         .then((user) => user.orders)
         .then((order) => order.items);
 }
 
-describe("example 1 normal", function(done){
+describe("get user's items monad", function(){
+    var users;
 
+    before("load fixture from fixture.json"){
+
+    });
+
+    if("return the same value from the normal and monad version", function(){
+        (get_users_items_normal(users) === get_users_items_monad(users))
+            .should.be.equal;
+    });
 });
 
 /*slide
-{example_1_monad}
+%(get_users_items_monad)s
 ???
-What pr
+Definition:
+- Container with one or more objects, empty can be thought of as null or none.
+- functions return a new list.
+- 'then' takes each item and passes it to the function then flattens results
+together.
 */
 
 /*slide
 Example 4. State Monad.
 
-
-
 ???
-All examples up to now
+All examples up to now have returned a simple value, however we don't have to
+return simple values,
 */
 
 function example_1_normal(cb) {
@@ -207,7 +267,7 @@ describe("example 1 normal", function(done){
 });
 
 /*slide
-{example_1_normal}
+%(example_1_normal)s
 */
 
 function example_1_monad() {
@@ -219,7 +279,7 @@ describe("example 1 normal", function(done){
 });
 
 /*slide
-{example_1_monad}
+%(example_1_monad)s
 ???
 What pr
 */
@@ -227,10 +287,11 @@ What pr
 /*slide
 Example 5. If/Else monad.
 ???
-Should be pretty familiar to everyone.
 */
 
-function example_1_normal(cb) {
+function guard(){}
+
+function if_else_normal(cb) {
     // TODO
 }
 
@@ -239,7 +300,7 @@ describe("example 1 normal", function(done){
 });
 
 /*slide
-{example_1_normal}
+%(if_else_normal)s
 */
 
 function example_1_monad() {
@@ -251,7 +312,58 @@ describe("example 1 normal", function(done){
 });
 
 /*slide
-{example_1_monad}
+%(example_1_monad)s
 ???
-What pr
+*/
+
+/*slide
+#What is a monad?
+
+Aside from how they look and how they meet the definition I provided there
+doesn't seem to be many commonalities...
+*/
+
+/*slide
+#What is a monad?
+
+Monads are an abstraction of computation itself.
+*/
+
+/*slide
+#What is a monad?
+
+Monads provide a way to create syntax for computations not built into the
+language.
+
+e.g. Async isn't built into javascript yet and other things will never be added
+as syntax in the language.
+*/
+
+/*slide
+#Some notes.
+
+Calling Monads a container or box isn't strictly correct, it's more like the
+context for your computation.
+
+e.g. The context for promises is more than just whether it has a value or
+failure. There are things like a TCP socket and event loop underneath.
+*/
+
+/*slide
+#Some notes.
+
+Although it's common, monads don't have to be sequential.
+
+e.g. the If/Else monad doesn't execute every step of the computation.
+*/
+
+/*slide
+#Some notes.
+
+Monads are part of a larger family of functional patterns: Functors,
+Applicatives, Monoids and more.
+*/
+
+/*slide
+Questions?
 */
